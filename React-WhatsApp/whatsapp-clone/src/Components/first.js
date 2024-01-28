75880f1bef538aa4d62686a470779ea7018c15d4
pradeepSharmadev <amarahipradeep7319@gmail.com>
2024-01-28 23:33:02 +0530

    ready to work

---
+++ React-WhatsApp/whatsapp-clone/src/Components/first.js
@@ -0,0 +1,13 @@
+
+function Massess(){
+    const name = "Pradeep kumar";
+    return(
+        <>
+        <h3>Welcome! to Clone-Project</h3>
+        <p>My name is {name}</p>
+        <p>This massess comes from componets</p>
+        </>
+    );
+};
+
+export default Massess;
